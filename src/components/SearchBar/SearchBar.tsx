bd54a9d530394fead47d12d34634810c779b285e
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-02-17 20:35:13 +0200

    Added input for searching

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,5 +1,17 @@
+import styles from "./SearchBar.module.css";
+import SearchIcon from "../../img/search.svg";
+
 function SearchBar() {
-  return <input type="text" placeholder="Search your trip" />;
+  return (
+    <form className={styles.imputForm}>
+      <img className={styles.imputIcon} src={SearchIcon} alt="" width={20} />
+      <input
+        className={styles.input}
+        type="text"
+        placeholder="Search your trip"
+      />
+    </form>
+  );
 }
 
 export default SearchBar;
